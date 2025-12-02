66a42306ecae13045f0f158e023c68d0f94ff9e0
davim5 <davim53@hotmail.com>
2025-12-01 23:24:58 -0300

    implementan rotas backend e incia frontend

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import axios from 'axios';
+
+interface UserI {
+  name: string;
+}
+
+const Home: React.FC = () => {
+  const [users, setUsers] = useState<UserI[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/users");
+        console.log(response.data);
+        setUsers(response.data);   // Ou response.data.users dependendo do backend
+      } catch (error) {
+        console.log("Erro na requisição de usuários", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Usuários</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user.name}</li>
+          ))}
+        </ul>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
